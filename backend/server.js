42c9fbe991952c4d6d2f5e727b713d2456f6f0e3
Tyler Malone <tylermalone@me.com>
2023-05-15 13:36:31 -0400

    Feat: finshied part one of guide figured out issue  (TBC-13)

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,14 @@ const source = process.env.ATLAS_CONNECTION;
 const mongoose = require("mongoose");
 mongoose.connect(source, {
   useNewUrlParser: true,
-  useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("DB connected.");
+});
+
+app.listen(() => {
+  console.log(`hello`);
 });
