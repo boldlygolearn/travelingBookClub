77341af86f733514a5da68657d9a9ee4947641d8
Tyler Malone <tylermalone@me.com>
2023-05-17 19:15:32 -0400

    feat:  create server with MongoDb, Mongoose and ExpressJS (TBC-13) (#6)
    * feat: create package json (TBC-13))
    
    * feat: add express package (TBC-13)
    
    * feat: add cors package (TBC-13)
    
    * feat: add dotenv package (TBC-13)
    
    * Feat: add mongoose packages (TBC-13)
    
    * feat: add nodemon packages (TBC-13)
    
    * feat: add server.js and code to connect it (TBC-13)
    
    * Feat: finshied part one of guide figured out issue  (TBC-13)
    
    * Feat: Making .env.sample and (TBC-13)

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+const source = process.env.ATLAS_CONNECTION;
+
+const mongoose = require("mongoose");
+mongoose.connect(source, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("DB connected.");
+});
+
+app.listen(() => {
+  console.log(`hello`);
+});
