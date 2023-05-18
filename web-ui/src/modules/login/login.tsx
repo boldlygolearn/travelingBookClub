bbb3203c631425ef9f09aa65b5f5a746d9d27cf0
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2023-05-18 13:16:08 -0700

    feat(login): create login page basics (TBC-22)

--- web-ui/src/modules/login/login.tsx
+++ web-ui/src/modules/login/login.tsx
@@ -1,7 +1,30 @@
-import { Typography } from "@mui/material";
+import { Button, Link, Stack, TextField, Typography } from "@mui/material";
 
 const Login = () => {
-  return <Typography>Login</Typography>;
+  return (
+    <Stack>
+      <Typography>Login</Typography>
+      <TextField
+        type="email"
+        label="Email"
+        sx={{ marginBottom: 4 }}
+      ></TextField>
+      <TextField
+        type="password"
+        label="Password"
+        sx={{ marginBottom: 4 }}
+      ></TextField>
+      <Button variant="contained">Login</Button>
+      <Typography>
+        Don't have an account?
+        {
+          <Link href="/create-account" underline="none">
+            Sign up
+          </Link>
+        }
+      </Typography>
+    </Stack>
+  );
 };
 
 export default Login;
