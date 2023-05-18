23558ac48c706c520760166577d21f2afbc02ec6
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2023-05-18 12:58:44 -0700

    feat: create login and new account routes and modules (TBC-22)

---
+++ web-ui/src/modules/login/login.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const Login = () => {
+  return <Typography>Login</Typography>;
+};
+
+export default Login;
---
+++ web-ui/src/modules/user/createUser/createUser.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const CreateUser = () => {
+  return <Typography>Create Account</Typography>;
+};
+
+export default CreateUser;
