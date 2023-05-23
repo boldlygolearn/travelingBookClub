27901be69f995e9256bfecc9dc10f925b58c2a73
Tyler Malone <tylermalone@me.com>
2023-05-23 10:55:56 -0400

    feat: made controllers and made auth file with created users pathway )

---
+++ backend/controllers/Auth.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  try {
+    const { firstName, lastName, email } = req.body;
+    if (!firstName || !lastName || !email) {
+      res.status(406).json({
+        message: "Invalid",
+      });
+      throw new Error("The user schema is not complete");
+    }
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+    });
+
+    await newUser.save();
+
+    res.status(200).json({
+      message: "User successfully create",
+      newUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `${error}`,
+    });
+  }
+});
