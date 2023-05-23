c29f68f52119c74d9a34d2f2508440e6dd216ba8
Tyler Malone <tylermalone@me.com>
2023-05-23 10:32:51 -0400

    feat: add models and creat user model (TBC-17)

---
+++ backend/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const User = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model("user", User);
