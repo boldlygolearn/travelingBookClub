66bcf776f9ca2efe7f03afedded6869985bc06ae
Tyler Malone <tylermalone@me.com>
2023-05-29 09:10:35 -0400

    feat: redid verification and update key and value pair (TBC-17)

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -9,19 +9,6 @@ const fields = {
 router.post("/register", async (req, res) => {
   try {
     const { firstName, lastName, email } = req.body;
-    if (!firstName || !lastName || !email) {
-      const missingFields = [];
-      console.log(Object.keys(req.body), "req.body");
-      Object.keys(fields).forEach((field) => {
-        console.log(field, "field");
-        if (!req.body[field]) {
-          console.log("test1");
-          missingFields.push(fields[field]);
-        }
-      });
-      console.log(missingFields);
-      throw new Error(`${missingFields.join()} is missing`);
-    }
     const newUser = new User({
       firstName,
       lastName,
@@ -29,12 +16,11 @@ router.post("/register", async (req, res) => {
     });
 
     await newUser.save();
-    const { _id } = newUser;
     res.status(201).json({
       message: "User successfully created",
       firstName,
       lastName,
-      _id,
+      id: newUser._id,
     });
   } catch (error) {
     res.status(500).json({
