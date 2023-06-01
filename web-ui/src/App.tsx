571005601cba542f762b02a536b9882114d1317a
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2023-06-01 15:56:16 -0500

    feat: install and configure AirBnb linter (TBC-23)

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Login from "./modules/login/login";
-import CreateUser from "./modules/user/createUser/createUser";
+import "./App.css"
+import { Route, Routes } from "react-router-dom"
+import Login from "./modules/login/login"
+import CreateUser from "./modules/user/createUser/createUser"
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/create-account" element={<CreateUser />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
