2248b5005329e2084f8ae2dad707cbdd0b6ad212
Tyler Malone <tylermalone@me.com>
2023-06-02 09:13:29 -0400

    feat: create user endpoint (TBC-17) (#9)
    create user end point

---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const User = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+  },
+});
+
+module.exports = mongoose.model("user", User);
