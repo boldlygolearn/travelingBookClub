89768abb2de8dd13b4973cca2550a039e9d39566
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2023-06-03 18:02:50 -0500

    feat: add validation (TBC-27)

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,28 @@
 const mongoose = require("mongoose");
-const User = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: true,
+    required: [true, "Please enter your first name"],
+    lowercase: true,
   },
   lastName: {
     type: String,
-    required: true,
+    required: [true, "Please enter your last name"],
+    lowercase: true,
   },
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter your email"],
     unique: true,
-    match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email"],
+    lowercase: true,
+    // validate: [()]
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: [8, "Minimum password length is 8 characters"],
   },
 });
 
-module.exports = mongoose.model("user", User);
+module.exports = mongoose.model("user", UserSchema);
