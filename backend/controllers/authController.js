193baf5dcdda0bdf2db046db7394b8669de6221d
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2023-06-10 13:08:20 -0700

    feat: add JWT response to login (TBC-28)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,19 @@
+const jwt = require('jsonwebtoken');
 const User = require("../models/User");
 
 const handleErrors = (err) => {
   if (err.code === 11000) {
     return { email: 'Email is already registered' };
   }
-return  Object.keys(err.errors).reduce((acc, key) => {
+
+  const errorKeys = Object.keys(err.errors)
+  if (errorKeys.length){
+    return {
+      message: "Unknown error"
+    }
+  }
+
+  return  errorKeys.reduce((acc, key) => {
     acc[key] = err.errors[key].message;
   return acc;
 }, {});
@@ -36,19 +45,31 @@ module.exports.register_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
-    if (!user) {
+    const loggedInUser = await User.findOne({ email });
+    if (!loggedInUser) {
       return res.status(404).json({
         email: "Email not found",
       });
     }
-    const passwordMatch = await user.matchPassword(password);
+    const passwordMatch = await loggedInUser.matchPassword(password);
     if (passwordMatch) {
+      const user = {
+        userId: loggedInUser._id,
+          firstName: loggedInUser.firstName,
+          lastName: loggedInUser.lastName,
+          email: loggedInUser.email,
+      }
+      const response = {
+        data: {
+          user
+        }
+      }
+      const token = jwt.sign({ ...response }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
       return res.status(200).json({
-        userId: user._id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
+        status: "success",
+        token,
       });
     } else {
       return res.status(401).json({
