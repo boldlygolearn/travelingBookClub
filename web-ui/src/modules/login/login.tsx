7888c310a27f938b3789bc883fdf6e201169fb1b
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2023-06-14 22:22:40 -0700

    feat: create login page with inputs (TBC-29)

--- web-ui/src/modules/login/login.tsx
+++ web-ui/src/modules/login/login.tsx
@@ -1,89 +1,101 @@
-import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { NavLink } from "react-router-dom";
-import { Link, Stack } from '@mui/material';
-
+import { useForm, Controller } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+import { NavLink } from "react-router-dom"
+import { Link, Stack, Typography } from "@mui/material"
 
 const schema = yup.object().shape({
-  email: yup
-    .string()
-    .required("Email is required")
-    .email("Email is not valid"),
+  email: yup.string().required("Email is required").email("Email is not valid"),
   password: yup
     .string()
     .required("Password is required")
-    .min(8, "Password must have at least 8 characters")
-});
+    .min(8, "Password must have at least 8 characters"),
+})
 
 type FormData = {
-  email: string;
-  password: string;
-};
-
-
+  email: string
+  password: string
+}
 
 const Login = () => {
-  const { control, handleSubmit, formState: { errors } } = useForm<FormData>({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
     resolver: yupResolver(schema),
     // mode: 'onChange',
-    mode: 'onSubmit',
-  });
+    mode: "onSubmit",
+  })
 
   const onSubmit = (data: FormData) => {
-    console.log(data);
-  };
+    console.log(data)
+  }
 
   return (
     <Stack>
+      <Stack mb={2}>
+        <Typography variant="h3" sx={{ alignSelf: "flex-start" }}>
+          The
+        </Typography>
+        <Typography variant="h1">Traveling Book</Typography>
+        <Typography variant="h3" sx={{ alignSelf: "flex-end" }}>
+          Club
+        </Typography>
+      </Stack>
       <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
-      <Controller
-        name="email"
-        control={control}
-        defaultValue=""
-        render={({ field }) =>
-          <TextField
-            {...field}
-            label="Email"
-            variant="outlined"
-            error={!!errors.email}
-            helperText={errors.email?.message}
-            fullWidth
-            margin="normal"
-          />
-        }
-      />
-
-      <Controller
-        name="password"
-        control={control}
-        defaultValue=""
-        render={({ field }) =>
-          <TextField
-            {...field}
-            type="password"
-            label="Password"
-            variant="outlined"
-            error={!!errors.password}
-            helperText={errors.password?.message}
-            fullWidth
-            margin="normal"
-          />
-        }
-      />
-
-      <Button type="submit" variant="contained" color="primary" fullWidth>
-        Login
-      </Button>
-    </form>
-      <Link to="/create-account" underline="none" component={NavLink}>
-        Create account
-      </Link>
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Email"
+              variant="outlined"
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              fullWidth
+              sx={{ mb: 2 }}
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              type="password"
+              label="Password"
+              variant="outlined"
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              fullWidth
+              sx={{ mb: 2 }}
+            />
+          )}
+        />
+        <Button type="submit" variant="contained" fullWidth sx={{ mb: 2 }}>
+          Login
+        </Button>
+      </form>
+      <Stack direction="row" justifyContent="center">
+        <Typography>Don't have an account?</Typography>
+        <Link
+          to="/create-account"
+          underline="none"
+          component={NavLink}
+          color="primary"
+        >
+          Create one
+        </Link>
+      </Stack>
     </Stack>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
