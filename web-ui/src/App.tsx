c73311b66d8667f5d0a89df765bf9ec8699a8b4f
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2023-06-14 22:22:22 -0700

    feat: update app to use theme (TBC-29)

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -2,13 +2,23 @@ import "./App.css"
 import { Route, Routes } from "react-router-dom"
 import Login from "./modules/login/login"
 import CreateUser from "./modules/user/createUser/createUser"
+import { ThemeProvider, useMediaQuery } from "@mui/material";
+import { useMemo } from "react";
+import { getTheme } from "./styles/theme";
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(() => getTheme(prefersDarkMode ? 'dark' : 'light'), [prefersDarkMode]);
+
   return (
+    <ThemeProvider theme={theme}>
+
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/create-account" element={<CreateUser />} />
     </Routes>
+    </ThemeProvider>
   )
 }
 
