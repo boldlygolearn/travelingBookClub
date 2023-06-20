9342007c9491e4715b72a97082d56f97055e18c6
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2023-06-19 20:32:33 -0700

    feat: move/remove files to correct locations (TBC-31)

--- backend/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const authController = require('../authentication/controllers/authController');
-
-router.post('/register', authController.register_post);
-router.post('/login', authController.login_post);
-
-module.exports = router;
