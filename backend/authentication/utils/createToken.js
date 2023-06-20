f46d22214b2d003933f47298504280494b4b49c0
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2023-06-19 20:33:44 -0700

    feat: move/remove files to correct locations (TBC-31) (#16)
    Move node modules and other files out of parent directory

--- backend/authentication/utils/createToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-}
-
-module.exports = createToken;
